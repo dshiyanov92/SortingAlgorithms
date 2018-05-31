540976dc26115e40f07125f63b5241f0ec34dfe4
iukarpov <karpoviu@gmail.com>
2018-05-31 23:00:56 +0300

    Добавьте файлы проекта.

---
+++ SortingAlgorithms/ArrayGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms
+{
+    class ArrayGenerator
+    {
+        
+      public int[] GenerateArray(int size)
+        {
+            switch (size){
+
+            }
+            Random n = new Random();
+            int[] array = new int[size];
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = n.Next(0, 2000000000);
+            }
+            return array;
+        }
+
+
+    }
+}
---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        enum Cases {Best, Average};
+        static void Main(string[] args)
+        {
+            ArrayGenerator sort = new ArrayGenerator();
+            int size = 100;
+            int[] testArray = sort.GenerateArray(size);
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            
+            // Сортировка выбором           
+            sw.Start();
+            ArraySort.SelectionSort(testArray);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+
+            //Сортировка вставками
+            testArray = sort.GenerateArray(size);
+            sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            ArraySort.InsertionSort(testArray);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            
+            //Сортировка Шелла
+            testArray = sort.GenerateArray(size);
+            sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            ArraySort.ShellSort(testArray);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+
+            //Быстрая сортировка
+            sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            ArraySort.Quicksort(testArray,0,testArray.Length-1);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+
+            //Сортировка слиянием
+            testArray = sort.GenerateArray(size);
+            sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            ArraySort.MergeSort(ref testArray,0,testArray.Length-1);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+
+            //Поразрядная сортировка
+            testArray = sort.GenerateArray(size);
+            sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            ArraySort.RadixSort(testArray,10,10);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+        }
+    }
+}
