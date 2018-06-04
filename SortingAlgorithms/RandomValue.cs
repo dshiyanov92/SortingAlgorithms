c2ddd7a8ce5d7baaffc6e55250b5f1f0a452a149
iukarpov <karpoviu@gmail.com>
2018-06-04 15:47:39 +0300

    New Review File added

--- SortingAlgorithms/RandomValue.cs
+++ SortingAlgorithms/RandomValue.cs
@@ -21,12 +21,14 @@ namespace SortingAlgorithms
                 string s3 = String.Concat("Количество элементов массива - ", s1);
                 File.WriteAllText(fileName, s3 + Environment.NewLine, System.Text.Encoding.Default);
 
+                System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
                 for (int j = 0; j < 10; j++)
                 {
                     File.AppendAllText(fileName, "Начало нового теста\n" + Environment.NewLine);
                     // Selection sort            
-                    int[] test1 = testArray;
-                    System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+                    int[] test1 = new int[arraySize[i]];
+                    Array.Copy(testArray, test1, testArray.Length);
                     sw = new System.Diagnostics.Stopwatch();
                     sw.Start();
                     ArraySort.SelectionSort(test1);
@@ -35,7 +37,8 @@ namespace SortingAlgorithms
                     File.AppendAllText(fileName, sw.Elapsed.TotalMilliseconds.ToString() + Environment.NewLine);
 
                     // Insertion sort
-                    int[] test2 = testArray;
+                    int[] test2 = new int[arraySize[i]];
+                    Array.Copy(testArray, test2, testArray.Length);
                     sw = new System.Diagnostics.Stopwatch();
                     sw.Start();
                     ArraySort.InsertionSort(test2);
@@ -44,7 +47,8 @@ namespace SortingAlgorithms
                     File.AppendAllText(fileName, sw.Elapsed.TotalMilliseconds.ToString() + Environment.NewLine);
 
                     // Shellsort
-                    int[] test3 = testArray;
+                    int[] test3 = new int[arraySize[i]];
+                    Array.Copy(testArray, test3, testArray.Length);
                     sw = new System.Diagnostics.Stopwatch();
                     sw.Start();
                     ArraySort.ShellSort(test3);
@@ -53,7 +57,8 @@ namespace SortingAlgorithms
                     File.AppendAllText(fileName, sw.Elapsed.TotalMilliseconds.ToString() + Environment.NewLine);
 
                     // Quicksort
-                    int[] test4 = testArray;
+                    int[] test4 = new int[arraySize[i]];
+                    Array.Copy(testArray, test4, testArray.Length);
                     sw = new System.Diagnostics.Stopwatch();
                     sw.Start();
                     ArraySort.Quicksort(test4, 0, test4.Length - 1);
@@ -62,7 +67,8 @@ namespace SortingAlgorithms
                     File.AppendAllText(fileName, sw.Elapsed.TotalMilliseconds.ToString() + Environment.NewLine);
 
                     // Merge sort
-                    int[] test5 = testArray;
+                    int[] test5 = new int[arraySize[i]];
+                    Array.Copy(testArray, test5, testArray.Length);
                     sw = new System.Diagnostics.Stopwatch();
                     sw.Start();
                     ArraySort.MergeSort(ref test5, 0, test5.Length - 1);
@@ -71,7 +77,8 @@ namespace SortingAlgorithms
                     File.AppendAllText(fileName, sw.Elapsed.TotalMilliseconds.ToString() + Environment.NewLine);
 
                     // RadixSort
-                    int[] test6 = testArray;
+                    int[] test6 = new int[arraySize[i]];
+                    Array.Copy(testArray, test6, testArray.Length);
                     sw = new System.Diagnostics.Stopwatch();
                     sw.Start();
                     ArraySort.RadixSort(test6, 10, 10);
